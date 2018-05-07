d0d7a373b6e43b65ade6a5a7afe530e11bbd4a5c
Saarstriker <simon.schoenhofen@gmail.com>
2018-05-07 17:12:43 +0200

    +added new Stuff

--- Python/MedBild.py
+++ Python/MedBild.py
@@ -1,85 +1,241 @@
-import os
 import numpy as np
-import SimpleITK as sitk
-#import matplotlib.pyplot as plt
-import cv2 as cv
+import pydicom
+import os
+import matplotlib.pyplot as plt
+from glob import glob
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import scipy.ndimage
+import scipy.ndimage
+from skimage import morphology
+from skimage import measure
+from skimage.transform import resize
+from sklearn.cluster import KMeans
+from plotly import __version__
+from plotly.offline import download_plotlyjs, plot, iplot
+from plotly.tools import FigureFactory as FF
+from plotly.graph_objs import *
 
 
-# def sitk_show(img, title=None, margin=0.05, dpi=40, normarlize=False):  # Shows the Image in a Plot
-#     nda = sitk.GetArrayFromImage(img)
-#     cv.normalize(nda, nda, 0, 257, cv.NORM_MINMAX)   # Map Values 0-257
-#     spacing = img.GetSpacing()
-#     figsize = (1 + margin) * nda.shape[0] / dpi, (1 + margin) * nda.shape[1] / dpi
-#     extent = (0, nda.shape[1] * spacing[1], nda.shape[0] * spacing[0], 0)
-#     fig = plt.figure(figsize=figsize, dpi=dpi)
-#     ax = fig.add_axes([margin, margin, 1 - 2 * margin, 1 - 2 * margin])
-#
-#     plt.set_cmap("gray")
-#     ax.imshow(nda, extent=extent, interpolation=None)
-#     print("max Value: " + str(np.max(nda)) + "min Value: " + str(np.min(nda)))
-#     if title:
-#         plt.title(title)
-#     plt.show()
-
-
-# def give_Dicom():
+# def give_path():
 #     print("Move the Folders with the Patient file into this Directory")
 #     patient = input("Enter the Patient Number (1-12)").lstrip()
 #     if len(patient) == 1:
 #         patient = '0' + patient
-#     path = "./P" + patient + "/"  # reads the path of the Dicom files
+#     path = "./P" + patient + "/"
+#     return path
+
+
+def make_mesh(image, threshold=-300, step_size=1):
+    print("Transposing surface")
+    p = image.transpose(2, 1, 0)
+
+    print("Calculating surface")
+    verts, faces = measure.marching_cubes_classic(p, threshold)
+    print("Done")
+    return verts, faces
+
+
+def plt_3d2(image, verts, faces, threshold=-300):
+    # Position the scan upright,
+    # so the head of the patient would be at the top facing the camera
+    p = image.transpose(2, 1, 0)
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Fancy indexing: `verts[faces]` to generate a collection of triangles
+    mesh = Poly3DCollection(verts[faces], alpha=0.70)
+    face_color = [0.45, 0.45, 0.75]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    ax.set_xlim(0, p.shape[0])
+    ax.set_ylim(0, p.shape[1])
+    ax.set_zlim(0, p.shape[2])
+
+    plt.show()
+
+
+def plt_3d(verts, faces):
+    print("Drawing")
+    x, y, z = zip(*verts)
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Fancy indexing: `verts[faces]` to generate a collection of triangles
+    mesh = Poly3DCollection(verts[faces], linewidths=0.05, alpha=1)
+    face_color = [1, 1, 0.9]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    ax.set_xlim(0, max(x))
+    ax.set_ylim(0, max(y))
+    ax.set_zlim(0, max(z))
+
+    plt.show()
+    print("done")
+
+
+def plotly_3d(verts, faces):
+    x, y, z = zip(*verts)
+
+    print("Drawing")
+
+    # Make the colormap single color since the axes are positional not intensity.
+    #    colormap=['rgb(255,105,180)','rgb(255,255,51)','rgb(0,191,255)']
+    colormap = ['rgb(236, 236, 212)', 'rgb(236, 236, 212)']
+
+    fig = FF.create_trisurf(x=x,
+                            y=y,
+                            z=z,
+                            plot_edges=False,
+                            colormap=colormap,
+                            simplices=faces,
+                            backgroundcolor='rgb(64, 64, 64)',
+                            title="Interactive Visualization")
+    plt.show()
+    print("Done")
+
+
+def plot_3d(image, threshold=-300):
+    print("Drawing...")
+    # Position the scan upright,
+    # so the head of the patient would be at the top facing the camera
+    p = image.transpose(2, 1, 0)
+
+    verts, faces = measure.marching_cubes_classic(p, threshold)
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Fancy indexing: `verts[faces]` to generate a collection of triangles
+    mesh = Poly3DCollection(verts[faces], alpha=0.70)
+    face_color = [0.45, 0.45, 0.75]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    ax.set_xlim(0, p.shape[0])
+    ax.set_ylim(0, p.shape[1])
+    ax.set_zlim(0, p.shape[2])
+
+    plt.show()
+
+
+# def scan_list(path):
 #     files = os.listdir(path)
 #     idxSlice = len(files)  # get the Number of Files in a Path (one Dicom Series)
+#     imgarray = []
 #
-#     reader = sitk.ImageSeriesReader()  # Loads the Dicom Reader
-#     filenamesDICOM = reader.GetGDCMSeriesFileNames(path)  # Load the Dicom files into the Reader
-#     reader.SetFileNames(filenamesDICOM)
-#     imgOriginal = reader.Execute()
-#
-#     for x in range(0, idxSlice):  # iterate through all the files (0-idxSlice)
-#         imgnew = imgOriginal[:, :, x]  # Display the current Picture
-#         # sitk_show(imgnew, 'Picture' + str(x + 1))
-#         # plt.pause(0.001)
-#         user = input("Press [enter] to continue.")  # Pressing enter gives the next image
-#         if user == 'end':  # Typing end stops the Programm
-#             return imgnew
-#         if user == 'this':  # Typing this opens the Image to work on it
-#             return imgnew
-#         plt.close()
-#     return imgnew
+#     for x in range(0, idxSlice):
+#         imgarray.append()
+
+def get_pixels_hu(slices):
+    image = np.stack([s.pixel_array for s in slices])
+    # Convert to int16 (from sometimes int16),
+    # should be possible as values should always be low enough (<32k)
+    image = image.astype(np.int16)
+
+    # Set outside-of-scan pixels to 0
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+
+    # Convert to Hounsfield units (HU)
+    for slice_number in range(len(slices)):
+
+        intercept = slices[slice_number].RescaleIntercept
+        slope = slices[slice_number].RescaleSlope
+
+        if slope != 1:
+            image[slice_number] = slope * image[slice_number].astype(np.float64)
+            image[slice_number] = image[slice_number].astype(np.int16)
+
+        image[slice_number] += np.int16(intercept)
+
+    return np.array(image, dtype=np.int16)
 
 
-def frame_image(image, low=0, high=100):
-    image = sitk.GetArrayFromImage(image)
-
-    for ix, iy in np.ndindex(image.shape):  # fensterung unten
-        if low < image[ix, iy] < high:
-            image[ix, iy] = 0
-    return sitk.GetImageFromArray(image)
+def data_path(x):
+    if len(x) == 1:
+        x = '0' + x
+    path = "./P" + x + "/"
+    return path
 
 
-def give_data():                                #returns an array of arrays witht the Image data
-    print("Move the Folders with the Patient file into this Directory")
-    patient = input("Enter the Patient Number (1-12)").lstrip()
-    if len(patient) == 1:
-        patient = '0' + patient
-    path = "./P" + patient + "/"  # reads the path of the Dicom files
-    files = os.listdir(path)
-    idxSlice = len(files)  # get the Number of Files in a Path (one Dicom Series)
+output_path = working_path = "./Docs"
 
-    reader = sitk.ImageSeriesReader()  # Loads the Dicom Reader
-    filenamesDICOM = reader.GetGDCMSeriesFileNames(path)  # Load the Dicom files into the Reader
-    reader.SetFileNames(filenamesDICOM)
-    imgOriginal = reader.Execute()
-    imgarray = []
-    for x in range(0, idxSlice):
-        pic = sitk.GetArrayFromImage(imgOriginal[:, :, x])
-        cv.normalize(pic, pic, 0, 257, cv.NORM_MINMAX)  # Werte zum Mappen/Skalieren (0-257)
-        imgarray.append(pic)
 
-    return imgarray
+def load_scan(path):
+    slices = [pydicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key=lambda x: float(x.ImagePositionPatient[2]))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+
+    for s in slices:
+        s.SliceThickness = slice_thickness
+
+    return slices
+
+
+def save_full_data(low, high):
+    for x in range(low, high):
+        patient = load_scan(data_path(str(x)))
+        imgs = get_pixels_hu(patient)
+        np.save(output_path + "fullimages_%d.npy" % (x), imgs)
+
+
+def give_histogramm(id):
+    file_used = output_path + "fullimages_%d.npy" % id
+    img_to_process = np.load(file_used).astype(np.float64)
+
+    plt.hist(img_to_process.flatten(), bins=50, color='c')
+    plt.xlabel("Houndsfield Units (HU)")
+    plt.ylabel("Frequency")
+    plt.show()
+
+
+def display_image_stack(stack, rows=6, cols=6, start_with=10, show_every=2):
+    fig, ax = plt.subplots(rows, cols, figsize=[12, 12])
+    for i in range(rows * cols):
+        ind = start_with + i * show_every - 1
+        ax[int(i / rows), int(i % rows)].set_title('slice %d' % ind)
+        ax[int(i / rows), int(i % rows)].imshow(stack[ind], cmap='gray')
+        ax[int(i / rows), int(i % rows)].axis('off')
+    plt.show()
+
+
+def resample(image, scan, new_spacing=[1, 1, 1]):
+    # Determine current pixel spacing
+    # spacing = np.array([scan[0].SliceThickness] + scan[0].PixelSpacing, dtype=np.float32)
+    print("Resampling image, putting the Layers Together")
+    spacing = np.array([scan[0].SliceThickness, scan[0].PixelSpacing[0], scan[0].PixelSpacing[1]],
+                       dtype=np.float32)
+    resize_factor = spacing / new_spacing
+    new_real_shape = image.shape * resize_factor
+    new_shape = np.round(new_real_shape)
+    real_resize_factor = new_shape / image.shape
+    new_spacing = spacing / real_resize_factor
+
+    image = scipy.ndimage.interpolation.zoom(image, real_resize_factor)
+    print("done")
+
+    return image, new_spacing
 
 
 if __name__ == "__main__":
-    img = give_data()
+    save_full_data(1, 12)  # EINMAL AUSFÜHREN, DANACH DIE ZEILE AUSKOMMENTIEREN
+    # give_histogramm(5)
+    id = 1
+    imgs_to_process = np.load(output_path + 'fullimages_{}.npy'.format(id))
+    display_image_stack(imgs_to_process)
+    patient = load_scan(data_path(str(1)))
+    # give_histogramm()
+    imgs_after_resamp, spacing = resample(imgs_to_process, patient, [1, 1, 1])
+    v, f = make_mesh(imgs_to_process, 350)
 
+    # Nur eine von den Funktionen bneutzen, Debug nötig
+    # plt_3d(v, f)
+    # plotly_3d(v, f)
+    # plt_3d2(imgs_after_resamp, v, f)
+    # plot_3d(imgs_after_resamp)
---
+++ Python/MedBildold.py
@@ -0,0 +1,101 @@
+import os
+import numpy as np
+import SimpleITK as sitk
+# import matplotlib.pyplot as plt
+import cv2 as cv
+
+
+# def sitk_show(img, title=None, margin=0.05, dpi=40, normarlize=False):  # Shows the Image in a Plot
+#     nda = sitk.GetArrayFromImage(img)
+#     cv.normalize(nda, nda, 0, 257, cv.NORM_MINMAX)   # Map Values 0-257
+#     spacing = img.GetSpacing()
+#     figsize = (1 + margin) * nda.shape[0] / dpi, (1 + margin) * nda.shape[1] / dpi
+#     extent = (0, nda.shape[1] * spacing[1], nda.shape[0] * spacing[0], 0)
+#     fig = plt.figure(figsize=figsize, dpi=dpi)
+#     ax = fig.add_axes([margin, margin, 1 - 2 * margin, 1 - 2 * margin])
+#
+#     plt.set_cmap("gray")
+#     ax.imshow(nda, extent=extent, interpolation=None)
+#     print("max Value: " + str(np.max(nda)) + "min Value: " + str(np.min(nda)))
+#     if title:
+#         plt.title(title)
+#     plt.show()
+
+
+# def give_Dicom():
+#     print("Move the Folders with the Patient file into this Directory")
+#     patient = input("Enter the Patient Number (1-12)").lstrip()
+#     if len(patient) == 1:
+#         patient = '0' + patient
+#     path = "./P" + patient + "/"  # reads the path of the Dicom files
+#     files = os.listdir(path)
+#     idxSlice = len(files)  # get the Number of Files in a Path (one Dicom Series)
+#
+#     reader = sitk.ImageSeriesReader()  # Loads the Dicom Reader
+#     filenamesDICOM = reader.GetGDCMSeriesFileNames(path)  # Load the Dicom files into the Reader
+#     reader.SetFileNames(filenamesDICOM)
+#     imgOriginal = reader.Execute()
+#
+#     for x in range(0, idxSlice):  # iterate through all the files (0-idxSlice)
+#         imgnew = imgOriginal[:, :, x]  # Display the current Picture
+#         # sitk_show(imgnew, 'Picture' + str(x + 1))
+#         # plt.pause(0.001)
+#         user = input("Press [enter] to continue.")  # Pressing enter gives the next image
+#         if user == 'end':  # Typing end stops the Programm
+#             return imgnew
+#         if user == 'this':  # Typing this opens the Image to work on it
+#             return imgnew
+#         plt.close()
+#     return imgnew
+
+def get_window_info(imagearr):  #
+    patient = input("Enter the Patient Number (1-12)").lstrip()
+    if len(patient) == 1:
+        patient = '0' + patient
+    path = "./P" + patient + "/"  # reads the path of the Dicom files
+    files = os.listdir(path)
+    idxSlice = len(files)  # get the Number of Files in a Path (one Dicom Series)
+
+    reader = sitk.ImageSeriesReader()  # Loads the Dicom Reader
+    filenamesDICOM = reader.GetGDCMSeriesFileNames(path)  # Load the Dicom files into the Reader
+    reader.SetFileNames(filenamesDICOM)
+    imgOriginal = reader.Execute()
+    for x in range(0, idxSlice):
+        pic = sitk.GetArrayFromImage(imgOriginal)
+
+
+def frame_image(image, low=0):
+    for ix, iy in np.ndindex(image.shape):  # fensterung unten
+        if low > image[ix, iy]:
+            image[ix, iy] = 0
+    return sitk.GetImageFromArray(image)
+
+
+def give_data():  # returns an array of arrays witht the Image data
+    print("Move the Folders with the Patient file into this Directory")
+    patient = input("Enter the Patient Number (1-12)").lstrip()
+    if len(patient) == 1:
+        patient = '0' + patient
+    path = "./P" + patient + "/"  # reads the path of the Dicom files
+    files = os.listdir(path)
+    idxSlice = len(files)  # get the Number of Files in a Path (one Dicom Series)
+
+    reader = sitk.ImageSeriesReader()  # Loads the Dicom Reader
+    filenamesDICOM = reader.GetGDCMSeriesFileNames(path)  # Load the Dicom files into the Reader
+    reader.SetFileNames(filenamesDICOM)
+    imgOriginal = reader.Execute()
+    imgarray = []
+    for x in range(0, idxSlice):
+        pic = sitk.GetArrayFromImage(imgOriginal[:, :, x])
+        # cv.normalize(pic, pic, 0, 257, cv.NORM_MINMAX)  # Werte zum Mappen/Skalieren (0-257)
+        imgarray.append(pic)
+
+    return imgarray, patient
+
+
+if __name__ == "__main__":
+    frame_down = [180, 250, 250, 230, 100, 220, ]
+    # idea: 1. get data(array of arrays) 2. window images 3. Map/Skale 0-257
+    img, patient = give_data()
+    img = frame_image(img, frame_down[patient - 1])
+    print(img)
